96d8ae76d2726f160b642ad984ff0cfc701b209f
sydnee510 <sbsydnee161416@gmail.com>
2020-02-17 14:20:09 -0800

    set up products controller and views

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,36 @@
 class ProductsController < ApplicationController
+    #before_action :set_product, only: [:show, :edit, :update, :destroy]
+    before_action :authenticate_user!
+    def index 
+        @products = Product.all 
+    end 
+    def show 
+        @product = Product.find(params[:id])
+    end 
+    def new 
+        @product = Product.new
+    end 
+    def edit 
+        @product = Product.find(params[:id])
+    end 
+    def create 
+        @product = Product.new(product_params)
+
+        # @product.save 
+        # redirect_to @product
+        if  @product.save 
+       #@product.valid?
+            redirect_to products_path, notice: "Product Successfully Created"
+        else 
+            render 'new'
+        end 
+    end 
+    def destroy 
+        @product = Product.find(params[:id]).destroy
+        redirect_to products_path
+    end 
+    private 
+    def product_params
+        params.require(:product).permit(:name, :description, :price, :category_id, :order_id, :shopping_cart_id)
+    end 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product <  ActiveRecord::Base
-    belongs_to :category
-    belongs_to :user
-    belongs_to :order
-    belongs_to :shopping_cart
+    belongs_to :category, optional: true
+    belongs_to :user, optional: true
+    belongs_to :order, optional: true
+    belongs_to :shopping_cart, optional: true
 end
