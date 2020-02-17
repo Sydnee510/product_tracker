f4de8cadf7f9759c7daed448bb7bb0e102822734
sydnee510 <sbsydnee161416@gmail.com>
2020-02-17 15:59:03 -0800

    can edit and delete objects, created nav bar

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,36 @@
-class CategoriesController < ApplicationController
+class CategoriesController < ProductsController
+    before_action :authenticate_user!
+    def index 
+        @categories = Category.all 
+    end 
+    def show 
+        @category = Category.find(params[:id])
+    end 
+    def new 
+        @category = Category.new
+    end 
+    def edit 
+        @category = Category.find(params[:id])
+    end 
+    def create 
+        @category = Category.new(category_params)
+
+        # @category.save 
+        # redirect_to @category
+        if  @category.save 
+       #@category.valid?
+            redirect_to categories_path, notice: "Category Successfully Created"
+        else 
+            render 'new'
+        end 
+    end 
+    def destroy 
+        @category = Category.find(params[:id]).destroy
+        redirect_to categories_path
+    end 
+    private 
+    def category_params
+        params.require(:category).permit(:title, :user_id)
+    end 
 end
+
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -29,6 +29,15 @@ class ProductsController < ApplicationController
         @product = Product.find(params[:id]).destroy
         redirect_to products_path
     end 
+    def update 
+        @product = Product.find(params[:id])
+        if @product.update(product_params)
+            redirect_to product_path(@product), notice: "product Successfully Updated" 
+        else 
+            render :edit 
+        end
+    end
+
     private 
     def product_params
         params.require(:product).permit(:name, :description, :price, :category_id, :order_id, :shopping_cart_id)
