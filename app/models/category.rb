5e31ee5df962466b0b31d7754d677d4b81b1a575
sydnee510 <sbsydnee161416@gmail.com>
2020-02-21 13:07:37 -0800

    added helper method to category modelso category.product_name will display each product name

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,11 +1,18 @@
 class Category < ApplicationRecord
     has_many :products
     has_many :users, through: :products 
-    
-    accepts_nested_attributes_for :products, reject_if: :all_blank
+
+    #accepts_nested_attributes_for :products, reject_if: :all_blank
 
    validates :title, presence: true, uniqueness:true
    
 
-   scope :search_category, ->(category_search) { where("title LIKE ?", "%#{category_search}%") }
+   
+    scope :search_category, ->(category_search) { where("title LIKE ?", "%#{category_search}%") }
+
+    def product_name 
+        self.products.map do |product| 
+        product.name
+        end 
+    end 
 end
